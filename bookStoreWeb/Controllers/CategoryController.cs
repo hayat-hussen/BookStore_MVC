e602e1ca24b14d77e649577f020fff86ed3dca83
hayat <hayathusen310@gmail.com>
2024-12-26 01:05:04 +0300

    Create Category Page With Validation

--- bookStoreWeb/Controllers/CategoryController.cs
+++ bookStoreWeb/Controllers/CategoryController.cs
@@ -1,21 +1,49 @@
-﻿using bookStoreWeb.Data;
-using bookStoreWeb.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using bookStoreWeb.Data; // Importing the data context for database operations
+using bookStoreWeb.Models; // Importing the model classes
+using Microsoft.AspNetCore.Mvc; // Importing ASP.NET Core MVC
 
-namespace bookStoreWeb.Controllers
+namespace bookStoreWeb.Controllers // Defining the namespace for the controller
 {
-    public class CategoryController : Controller
+    public class CategoryController : Controller // Creating the CategoryController class
     {
-        private readonly ApplicationDbContext _db;
+        private readonly ApplicationDbContext _db; // Declaring a private variable for the database context
 
+        // Constructor to initialize the database context
         public CategoryController(ApplicationDbContext db)
         {
-            _db = db;  
+            _db = db; // Assigning the passed database context to the private variable
         }
+
+        // Action method to display the list of categories
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
-            return View(objCategoryList);
+            List<Category> objCategoryList = _db.Categories.ToList(); // Getting the list of categories from the database
+            return View(objCategoryList); // Returning the list to the view
+        }
+
+        // Action method to show the create category form
+        public IActionResult Create()
+        {
+            return View(); // Returning the view for creating a new category
+        }
+
+        // Action method to handle the form submission for creating a category
+        [HttpPost] // This method will respond to POST requests
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name== obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name","The Display Cant Be Exacly The Same With The Name");
+            }
+
+            if (ModelState.IsValid) 
+            { 
+            _db.Categories.Add(obj); // Adding the new category to the database
+            _db.SaveChanges(); // Saving the changes to the database
+                              
+            return RedirectToAction("Index", "Category");
+            }
+            return View();
         }
     }
 }
--- bookStoreWeb/Models/Category.cs
+++ bookStoreWeb/Models/Category.cs
@@ -1,14 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel; // For DisplayName attribute
+using System.ComponentModel.DataAnnotations; // For validation attributes
 
-namespace bookStoreWeb.Models
+namespace bookStoreWeb.Models // Namespace for the model
 {
-    public class Category
-    {// prop  category class
-        // to make id primary [key],or Category__
-        [Key]
-        public int  Id { get; set; }
-        [Required]
+    public class Category // Category class
+    {
+        [Key] // Primary key
+        public int Id { get; set; }
+
+        [Required] // Must be provided
+        [DisplayName("Category Name")] // Display name
+        [MaxLength(30)] // Max length of 30 characters
         public string Name { get; set; }
+
+        [DisplayName("Display Order")] // Display name
+        [Range(1, 100)] // Value must be between 1 and 100
         public int DisplayOrder { get; set; }
     }
 }
